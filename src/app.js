c2d09869d5339b53739e95883662333eaa0afe50
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-11-16 15:07:57 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,126 @@
+var chocolates = [
+  'green',
+  'green',
+  'green',
+  'silver',
+  'blue',
+  'crimson',
+  'purple',
+  'red',
+  'crimson',
+  'purple',
+  'purple',
+  'green',
+  'pink',
+  'blue',
+  'red',
+  'silver',
+  'crimson',
+  'purple',
+  'silver',
+  'silver',
+  'red',
+  'green',
+  'red',
+  'silver',
+  'pink',
+  'crimson',
+  'purple',
+  'green',
+  'red',
+  'silver',
+  'crimson',
+  'pink',
+  'silver',
+  'blue',
+  'pink',
+  'crimson',
+  'crimson',
+  'crimson',
+  'red',
+  'purple',
+  'purple',
+  'green',
+  'pink',
+  'blue',
+  'red',
+  'crimson',
+  'silver',
+  'purple',
+  'purple',
+  'purple',
+  'red',
+  'purple',
+  'red',
+  'blue',
+  'silver',
+  'green',
+  'crimson',
+  'silver',
+  'blue',
+  'crimson',
+  'purple',
+  'green',
+  'pink',
+  'green',
+  'red',
+  'silver',
+  'crimson',
+  'blue',
+  'green',
+  'red',
+  'red',
+  'pink',
+  'blue',
+  'silver',
+  'pink',
+  'crimson',
+  'purple',
+  'green',
+  'red',
+  'blue',
+  'red',
+  'purple',
+  'silver',
+  'blue',
+  'pink',
+  'silver',
+  'crimson',
+  'silver',
+  'blue',
+  'purple',
+  'purple',
+  'green',
+  'blue',
+  'blue',
+  'red',
+  'red',
+  'silver',
+  'purple',
+  'silver',
+  'crimson',
+];
+
+// x and y ==> can take any of the values from the below list-
+// [ green, red, purple, blue, crimson, silver, pink ]
+// z ==> can take a number from 0<=a<=100
+
+//Progression 1: Add z chocolates of x color
+
+//Progression 2: Remove z chocolates from the top the dispenser
+
+//Progression 3: Dispense z chocolates
+
+//Progression 4: Dispense z chocolates of x color
+
+//Progression 5: Display z chocolates of each color. Return array of numbers [green, silver, blue, crimson, purple, red, pink]
+
+//Progression 6: Sort chocolates based on count in each color. Return array of colors
+
+//Progression 7: Change z chocolates of x color to y color
+
+//Progression 8: Change all chocolates of x color to y color and return [countOfChangedColor, chocolates]
+
+//Challenge 1: Remove one chocolate of x color from the top
+
+//Challenge 2: Dispense 1 rainbow colored colored chocolate for every 3 chocolates of the same color dispensed
